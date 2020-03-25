62009fd741003826a87bebff61eb1b817d005e4b
DiegoJRR <d.ramirez@hlcn.mx>
2020-03-24 18:03:57 -0600

    Added experiments folder and jupyter notebook

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 from secret.keys import api_key, api_secret
 
-settings_dict = {'dt': 1,
+settings_dict = {'dt': 10,
             'api_key': api_key,
             'api_secret': api_secret,
             'resolution': 300,
             'window': 'day',
-            'market': 'ETHBULL/USD',
+            'market': 'BULL/USD',
             'state': 'stable',
-            'balance': {'USD': 25, 'ETHBULL': 0.0},
+            'balance': {'USD': 25, 'BULL': 0.0},
             'demo': True,
             'minimum_roi': 0.05}
